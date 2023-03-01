d2c2ddd6a03a415cb0dda95cb4c907f3e813b454
theniftycat <126675135+theniftycat@users.noreply.github.com>
2023-03-01 22:22:45 +0700

    Update tic-tac-toe.py

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -6,8 +6,8 @@ F = [[" ", "1", "2", "3"],
 for line in F:
     print ('  '.join(map(str, line)))
 # ask Player 1 to place X
-n=0
-while n < 9:
+n = 1
+while True:
     print("Player 1, make a move! First, select a row.")
     a = int(input())
     while a < 1 or a > 3:
@@ -33,9 +33,11 @@ while n < 9:
     n += 1
     for line in F:
         print ('  '.join(map(str, line)))
+    if n > 9:
+        print("Looks like there's no winners!")
+        break
     if (F[1][1] == "X" and F[1][2] == "X" and F[1][3] == "X") or (F[2][1] == "X" and F[2][2] == "X" and F[2][3] == "X") or (F[3][1] == "X" and F[3][2] == "X" and F[3][3] == "X") or (F[1][1] == "X" and F[2][1] == "X" and F[3][1] == "X") or (F[1][2] == "X" and F[2][2] == "X" and F[3][2] == "X") or (F[1][3] == "X" and F[2][3] == "X" and F[3][3] == "X") or (F[1][1] == "X" and F[2][2] == "X" and F[3][3] == "X") or (F[3][1] == "X" and F[2][2] == "X" and F[1][3] == "X"):
         print("Player 1 won!")
-        n = 10
         break
 
     # ask Player 2 to place O
@@ -66,5 +68,4 @@ while n < 9:
         print ('  '.join(map(str, line)))
     if (F[1][1] == "O" and F[1][2] == "O" and F[1][3] == "O") or (F[2][1] == "O" and F[2][2] == "O" and F[2][3] == "O") or (F[3][1] == "O" and F[3][2] == "O" and F[3][3] == "O") or (F[1][1] == "O" and F[2][1] == "O" and F[3][1] == "O") or (F[1][2] == "O" and F[2][2] == "O" and F[3][2] == "O") or (F[1][3] == "O" and F[2][3] == "O" and F[3][3] == "O") or (F[1][1] == "O" and F[2][2] == "O" and F[3][3] == "O") or (F[3][1] == "O" and F[2][2] == "O" and F[1][3] == "O"):
         print("Player 2 won!")
-        n = 10
-
+        break
